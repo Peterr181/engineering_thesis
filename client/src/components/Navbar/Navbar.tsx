e28b76f066c8d04f64db062bb00e03eac5e67e73
Peterr181 <piotrzerdzinski089@gmail.com>
2023-12-21 11:49:15 +0100

    added initial i18n language switcher configuration

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,35 +1,44 @@
-import React from "react";
+// Navbar.js
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
 import styles from "./Navbar.module.scss";
 import { iconFile } from "../../assets/iconFile";
 import humanFace from "../../assets/images/personDefault.jpg";
 import Button from "../Button/Button";
 
 const Navbar = () => {
+  const [t, i18n] = useTranslation("global");
+
+  const [currentLanguage, setCurrentLanguage] = useState(i18n.language);
+  const toggleLanguage = () => {
+    const newLanguage = currentLanguage === "en" ? "pl" : "en";
+    i18n.changeLanguage(newLanguage);
+    setCurrentLanguage(newLanguage);
+  };
   return (
     <nav className={styles.navbar__wrapper}>
       <section className={styles.navbar}>
         <div className={styles.navbar__left}>
-          <p>Set your gym workout plan</p>
-          <Button variant="primaryOutline">Set workout plan</Button>
+          <p>{t("navbar.setWorkoutPlan")}</p>
+          <Button variant="primaryOutline">
+            {t("navbar.setWorkoutButton")}
+          </Button>
         </div>
         <div className={styles.navbar__right}>
-          {/* <div className={styles.navbar__right__input}>
-          {iconFile.searchIcon}
-          <input type="text" placeholder="Search something..." />
-        </div> */}
           <div className={styles.navbar__right__icons}>
             <span>{iconFile.moonIcon}</span>
             <span>{iconFile.notifyIcon}</span>
             <span>{iconFile.chatIcon}</span>
             <span>{iconFile.giftIcon}</span>
+            <span onClick={toggleLanguage}>{iconFile.languageIcon}</span>
           </div>
           <div className={styles.navbar__right__profile}>
             <div>
-              <img src={humanFace} alt="Human face" />
+              <img src={humanFace} alt={t("navbar.humanFaceAlt")} />
             </div>
             <div>
-              <p>Peter Morgan</p>
-              <span>Member</span>
+              <p>{t("navbar.userName")}</p>
+              <span>{t("navbar.userRole")}</span>
             </div>
           </div>
         </div>
