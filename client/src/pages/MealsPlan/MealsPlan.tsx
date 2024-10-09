4c106c5b5ad98c71122e576550541e0010e390fa
Peterr181 <piotrzerdzinski089@gmail.com>
2024-10-09 13:56:32 +0200

    add initial diet plan working adding meals deleting meals and calculating calories and more

--- client/src/pages/MealsPlan/MealsPlan.tsx
+++ client/src/pages/MealsPlan/MealsPlan.tsx
@@ -1,49 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./MealsPlan.module.scss";
 import PlatformWrapper from "../../components/compound/PlatformWrapper/PlatformWrapper";
 import MaxWidthWrapper from "../../components/compound/MaxWidthWrapper/MaxWidthWrapper";
 import WhiteCardWrapper from "../../components/atomic/WhiteCardWrapper/WhiteCardWrapper";
-
-interface Meal {
-  name: string;
-  calories: number;
-  protein: number;
-  carbs: number;
-  fats: number;
-}
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import ButtonMy from "../../components/atomic/Button/Button";
+import { iconFile } from "../../assets/iconFile";
+import { useMeals } from "../../hooks/useMeals";
+import axios from "axios";
+import IconButton from "@mui/material/IconButton";
 
 const MealsPlan = () => {
-  // Dummy data for meals
-  const [meals] = useState({
-    breakfast: [
-      { name: "Oatmeal", calories: 150, protein: 5, carbs: 27, fats: 3 },
-      { name: "Boiled Eggs", calories: 140, protein: 12, carbs: 1, fats: 10 },
-    ],
-    lunch: [
-      {
-        name: "Grilled Chicken",
-        calories: 300,
-        protein: 30,
-        carbs: 0,
-        fats: 12,
-      },
-      { name: "Salad", calories: 100, protein: 3, carbs: 10, fats: 7 },
-    ],
-    dinner: [
-      { name: "Steak", calories: 500, protein: 45, carbs: 0, fats: 35 },
-      { name: "Sweet Potatoes", calories: 200, protein: 4, carbs: 45, fats: 0 },
-    ],
-    snacks: [
-      { name: "Greek Yogurt", calories: 120, protein: 10, carbs: 12, fats: 5 },
-      { name: "Almonds", calories: 250, protein: 8, carbs: 10, fats: 20 },
-    ],
-    supper: [
-      { name: "Cottage Cheese", calories: 100, protein: 15, carbs: 5, fats: 4 },
-    ],
-  });
-
-  const mealSections = ["breakfast", "lunch", "dinner", "snacks", "supper"];
-
   const [openSections, setOpenSections] = useState({
     breakfast: false,
     lunch: false,
@@ -52,11 +23,105 @@ const MealsPlan = () => {
     supper: false,
   });
 
+  const [openDialog, setOpenDialog] = useState(false);
+  const [selectedMealType, setSelectedMealType] = useState<string | null>(null);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [isMealAdded, setIsMealAdded] = useState(false);
+
+  const { meals, fetchMeals, addMeal, deleteMeal, error, loading } = useMeals();
+  console.log(isMealAdded, "posilek dodany");
+  useEffect(() => {
+    fetchMeals();
+  }, [isMealAdded]);
+
   const toggleSection = (section: string) => {
     setOpenSections((prevState) => ({
       ...prevState,
       [section]: !prevState[section],
     }));
+  };
+
+  const handleOpenDialog = (mealType: string) => {
+    setSelectedMealType(mealType);
+    setOpenDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+    setSearchResults([]);
+    setSearchQuery("");
+  };
+
+  const handleSearchQueryChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    if (searchQuery.trim() === "") return;
+    try {
+      const response = await axios.get(
+        `https://api.edamam.com/api/food-database/v2/parser`,
+        {
+          params: {
+            app_id: import.meta.env.VITE_REACT_APP_MEALS_API_ID,
+            app_key: import.meta.env.VITE_REACT_APP_MEALS_API_KEY,
+            ingr: searchQuery,
+          },
+        }
+      );
+      const fetchedMeals = response.data.hints.map((hint: any) => ({
+        name: hint.food.label,
+        calories: hint.food.nutrients.ENERC_KCAL || 0,
+        protein: hint.food.nutrients.PROCNT || 0,
+        carbs: hint.food.nutrients.CHOCDF || 0,
+        fats: hint.food.nutrients.FAT || 0,
+      }));
+      setSearchResults(fetchedMeals);
+    } catch (error) {
+      console.error("Error fetching meals:", error);
+    }
+  };
+
+  const handleAddMeal = (meal: Meal) => {
+    if (selectedMealType) {
+      addMeal({
+        ...meal,
+        type: selectedMealType,
+      });
+      setIsMealAdded(true);
+      handleCloseDialog();
+    }
+  };
+
+  const calculateTotals = (mealType: string) => {
+    const mealTypeMeals = meals.filter((meal) => meal.type === mealType);
+
+    const totalCalories = Math.round(
+      mealTypeMeals.reduce((total, meal) => total + meal.calories, 0)
+    );
+    const totalProtein = Math.round(
+      mealTypeMeals.reduce((total, meal) => total + meal.protein, 0)
+    );
+    const totalCarbs = Math.round(
+      mealTypeMeals.reduce((total, meal) => total + meal.carbs, 0)
+    );
+    const totalFats = Math.round(
+      mealTypeMeals.reduce((total, meal) => total + meal.fats, 0)
+    );
+
+    return { totalCalories, totalProtein, totalCarbs, totalFats };
+  };
+
+  const handleDeleteMeal = async (mealId: string) => {
+    try {
+      await deleteMeal(mealId);
+      setIsMealAdded(true);
+    } catch (error) {
+      console.error("Error deleting meal:", error);
+    }
   };
 
   return (
@@ -70,51 +135,153 @@ const MealsPlan = () => {
               on your preferences and goals.
             </p>
 
-            {mealSections.map((section) => (
-              <div key={section} className={styles.mealSection}>
-                <div
-                  className={styles.sectionHeader}
-                  onClick={() => toggleSection(section)}
-                >
-                  <div className={styles.mealHeader}>
-                    <h3>
-                      {section.charAt(0).toUpperCase() + section.slice(1)}
-                    </h3>
-                    <span className={styles.arrow}>
-                      {openSections[section] ? "▼" : "►"}
-                    </span>
-                  </div>
-                  {/* Nutrition Totals */}
-                  <div className={styles.totals}>
-                    <p>Calories: 22</p>
-                    <p>Protein: 33g</p>
-                    <p>Carbs: 44g</p>
-                    <p>Fats: 55g</p>
-                  </div>
-                </div>
+            {["breakfast", "lunch", "dinner", "snacks", "supper"].map(
+              (section) => {
+                const totals = calculateTotals(section);
+                return (
+                  <div key={section} className={styles.mealSection}>
+                    <div className={styles.sectionHeader}>
+                      <div
+                        className={styles.mealHeader}
+                        onClick={() => toggleSection(section)}
+                      >
+                        <h3>
+                          {section.charAt(0).toUpperCase() + section.slice(1)}
+                        </h3>
 
-                <ul
-                  className={`${styles.mealList} ${
-                    openSections[section] ? styles.show : ""
-                  }`}
-                >
-                  {meals[section].map((meal, index) => (
-                    <li key={index} className={styles.mealItem}>
-                      <div>
-                        <h3>{meal.name}</h3>
+                        {meals.filter((meal) => meal.type === section).length >
+                        0 ? (
+                          iconFile.arrowDown
+                        ) : (
+                          <Button
+                            variant="contained"
+                            color="success"
+                            onClick={() => {
+                              handleOpenDialog(section);
+                              setIsMealAdded(false);
+                            }}
+                          >
+                            Add Meal
+                          </Button>
+                        )}
                       </div>
-                      <div className={styles.mealCalories}>
-                        <span>{meal.calories} kcal</span>
-                        <span>Protein: {meal.protein}g</span>
-                        <span>Carbs: {meal.carbs}g</span>
-                        <span>Fats: {meal.fats}g</span>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
+                    </div>
+
+                    <div className={styles.totalsSummary}>
+                      {totals.totalCalories} kcal, {totals.totalProtein}g
+                      Protein, {totals.totalCarbs}g Carbs, {totals.totalFats}g
+                      Fats
+                    </div>
+
+                    {meals.filter((meal) => meal.type === section).length >
+                    0 ? (
+                      <ul
+                        className={`${styles.mealList} ${
+                          openSections[section] ? styles.show : ""
+                        }`}
+                      >
+                        {meals
+                          .filter((meal) => meal.type === section)
+                          .map((meal, index) => (
+                            <li key={index} className={styles.mealItem}>
+                              <div className={styles.mealItem__header}>
+                                <h3>{meal.name}</h3>
+                                <span onClick={() => handleDeleteMeal(meal.id)}>
+                                  {iconFile.circleMinus}
+                                </span>
+                              </div>
+                              <div className={styles.mealCalories}>
+                                <span className={styles.mealCalories__calories}>
+                                  {Math.round(meal.calories)} kcal
+                                </span>
+                                <span className={styles.mealCalories__protein}>
+                                  Protein: {Math.round(meal.protein)}g
+                                </span>
+                                <span className={styles.mealCalories__carbs}>
+                                  Carbs: {Math.round(meal.carbs)}g
+                                </span>
+                                <span className={styles.mealCalories__fats}>
+                                  Fats: {Math.round(meal.fats)}g
+                                </span>
+                              </div>
+                            </li>
+                          ))}
+                        <div className={styles.addButton}>
+                          <Button
+                            variant="contained"
+                            color="success"
+                            onClick={() => {
+                              handleOpenDialog(section);
+                              setIsMealAdded(false);
+                            }}
+                          >
+                            Add Meal
+                          </Button>
+                        </div>
+                      </ul>
+                    ) : null}
+                  </div>
+                );
+              }
+            )}
           </div>
+          <Dialog
+            open={openDialog}
+            onClose={handleCloseDialog}
+            fullWidth={true}
+          >
+            <DialogTitle align="center" fontWeight={500}>
+              Search for a meal that you want to add
+            </DialogTitle>
+            <DialogContent>
+              <input
+                type="text"
+                value={searchQuery}
+                onChange={handleSearchQueryChange}
+                placeholder="Search for a meal..."
+                className={styles.searchInput}
+              />
+              <div className={styles.buttonSearch}>
+                <ButtonMy variant="filter" onClick={handleSearch}>
+                  Search
+                </ButtonMy>
+              </div>
+              <ul className={styles.searchResults}>
+                {searchResults.map((meal, index) => (
+                  <li key={index} className={styles.mealItem}>
+                    <div>
+                      <h3>{meal.name}</h3>
+                      <div className={styles.mealCalories}>
+                        <span className={styles.mealCalories__calories}>
+                          {Math.round(meal.calories)} kcal
+                        </span>
+                        <span className={styles.mealCalories__protein}>
+                          Protein: {Math.round(meal.protein)}g
+                        </span>
+                        <span className={styles.mealCalories__carbs}>
+                          Carbs: {Math.round(meal.carbs)}g
+                        </span>
+                        <span className={styles.mealCalories__fats}>
+                          Fats: {Math.round(meal.fats)}g
+                        </span>
+                      </div>
+                    </div>
+                    <Button
+                      variant="contained"
+                      onClick={() => handleAddMeal(meal)}
+                    >
+                      Add
+                    </Button>
+                  </li>
+                ))}
+              </ul>
+            </DialogContent>
+            <div>
+              <DialogActions>
+                <Button onClick={handleCloseDialog}>Close</Button>
+              </DialogActions>
+            </div>
+          </Dialog>
         </WhiteCardWrapper>
       </MaxWidthWrapper>
     </PlatformWrapper>
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { config } from "dotenv";
 
+import mealsRoutes from "./routes/mealsRoutes.js";
 import workoutRoutes from "./routes/workoutRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use("/auth", authRoutes);
 app.use("/user", userRoutes);
 app.use("/api/workouts", workoutRoutes);
+app.use("/api/meals", mealsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
