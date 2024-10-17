e7d9800a79e47756b55bf210875fa58b36946282
Peterr181 <piotrzerdzinski089@gmail.com>
2024-10-17 23:35:15 +0200

    display total amount of calories progress

--- client/src/components/compound/MealsSummary/MealsSummary.tsx
+++ client/src/components/compound/MealsSummary/MealsSummary.tsx
@@ -5,13 +5,25 @@ import { useMeals } from "../../../hooks/useMeals";
 import WhiteCardWrapper from "../../atomic/WhiteCardWrapper/WhiteCardWrapper";
 import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import { usePersonalInfo } from "../../../hooks/usePersonalInfo";
 
 const MealsSummary = () => {
   const { fetchMeals, mealSummaryData } = useMeals();
+  const { personalInfoData } = usePersonalInfo();
 
   useEffect(() => {
     fetchMeals();
   }, []);
+
+  const caloricIntakeGoal = parseInt(
+    personalInfoData?.find((item) => item.label === "caloric_intake_goal")
+      ?.value ?? "0",
+    10
+  );
+
+  const proteinGoal = (caloricIntakeGoal * 0.3) / 4;
+  const carbsGoal = (caloricIntakeGoal * 0.4) / 4;
+  const fatsGoal = (caloricIntakeGoal * 0.3) / 9;
 
   return (
     <div className={styles.mealssummary}>
@@ -30,25 +42,45 @@ const MealsSummary = () => {
               </div>
             </Link>
           </div>
-          {mealSummaryData ? ( // Use mealSummaryData directly
+
+          {mealSummaryData ? (
             <div className={styles.mealCalories}>
               <div className={styles.mealCalories__total}>
-                <span className={styles.mealCalories__calories}>
-                  {Math.round(mealSummaryData.totalCalories)} kcal
-                </span>
-                <span className={styles.mealCalories__protein}>
-                  {Math.round(mealSummaryData.totalProtein)}g Protein
-                </span>
-                <span className={styles.mealCalories__carbs}>
-                  {Math.round(mealSummaryData.totalCarbs)}g Carbs
-                </span>
-                <span className={styles.mealCalories__fats}>
-                  {Math.round(mealSummaryData.totalFats)}g Fats
-                </span>
+                <div className={styles.mealCalories__item}>
+                  <span className={styles.mealCalories__label}>kcal</span>
+                  <span className={styles.mealCalories__calories}>
+                    {Math.round(mealSummaryData.totalCalories)} /{" "}
+                    {caloricIntakeGoal}
+                  </span>
+                </div>
+
+                <div className={styles.mealCalories__item}>
+                  <span className={styles.mealCalories__label}>Protein</span>
+                  <span className={styles.mealCalories__protein}>
+                    {Math.round(mealSummaryData.totalProtein)}g /{" "}
+                    {Math.round(proteinGoal)}g
+                  </span>
+                </div>
+
+                <div className={styles.mealCalories__item}>
+                  <span className={styles.mealCalories__label}>Carbs</span>
+                  <span className={styles.mealCalories__carbs}>
+                    {Math.round(mealSummaryData.totalCarbs)}g /{" "}
+                    {Math.round(carbsGoal)}g
+                  </span>
+                </div>
+
+                <div className={styles.mealCalories__item}>
+                  <span className={styles.mealCalories__label}>Fats</span>
+                  <span className={styles.mealCalories__fats}>
+                    {Math.round(mealSummaryData.totalFats)}g /{" "}
+                    {Math.round(fatsGoal)}g
+                  </span>
+                </div>
               </div>
             </div>
           ) : (
-            <p>Loading...</p> // Display a loading message if data is not yet available
+            <p>Loading...</p>
           )}
         </MaxWidthWrapper>
       </WhiteCardWrapper>
