62fcd44bb7616dfdc7d4b38aae21037c191f2ef3
Peterr181 <piotrzerdzinski089@gmail.com>
2024-10-21 10:25:49 +0200

    switch from bcrypt to bcryptjs

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs"; // Use bcryptjs
 import jwt from "jsonwebtoken";
 import mysql from "mysql";
 
@@ -11,18 +11,23 @@ const db = mysql.createConnection({
 
 const saltRounds = 10;
 
+const handleError = (res, status, message) => {
+  return res.status(status).json({ error: message });
+};
+
 export const register = (req, res) => {
   const { nickname, password, email, gender, birthYear, avatar, sportLevel } =
     req.body;
 
   const checkEmailSql = "SELECT * FROM users WHERE email = ?";
   db.query(checkEmailSql, [email], (err, result) => {
-    if (err) return res.status(500).json({ error: "Internal Server Error" });
+    if (err) return handleError(res, 500, "Internal Server Error");
+
     if (result.length > 0)
-      return res.status(400).json({ error: "Email already registered" });
+      return handleError(res, 400, "Email already registered");
 
     bcrypt.hash(password.toString(), saltRounds, (err, hash) => {
-      if (err) return res.status(500).json({ error: "Error hashing password" });
+      if (err) return handleError(res, 500, "Error hashing password");
 
       const sql = `INSERT INTO users (username, password, email, gender, birthYear, avatar, sportLevel) VALUES (?, ?, ?, ?, ?, ?, ?)`;
       const values = [
@@ -36,16 +41,13 @@ export const register = (req, res) => {
       ];
 
       db.query(sql, values, (err, result) => {
-        if (err)
-          return res.status(500).json({ error: "Error registering user" });
+        if (err) return handleError(res, 500, "Error registering user");
 
         const user_id = result.insertId;
         const token = jwt.sign(
           { id: user_id, username: nickname },
           process.env.JWT_SECRET,
-          {
-            expiresIn: "1d",
-          }
+          { expiresIn: "1d" }
         );
 
         return res.status(200).json({
@@ -63,15 +65,15 @@ export const login = (req, res) => {
 
   const sql = "SELECT * FROM users WHERE username = ?";
   db.query(sql, [username], (err, data) => {
-    if (err) return res.status(500).json({ error: "Internal Server Error" });
-    if (data.length === 0)
-      return res.status(404).json({ error: "User not found" });
+    if (err) return handleError(res, 500, "Internal Server Error");
+
+    if (data.length === 0) return handleError(res, 404, "User not found");
 
     const user = data[0];
     bcrypt.compare(password.toString(), user.password, (err, isMatch) => {
-      if (err) return res.status(500).json({ error: "Internal Server Error" });
-      if (!isMatch)
-        return res.status(401).json({ error: "Invalid credentials" });
+      if (err) return handleError(res, 500, "Internal Server Error");
+
+      if (!isMatch) return handleError(res, 401, "Invalid credentials");
 
       const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
         expiresIn: "1d",
